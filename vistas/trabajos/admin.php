a3e96f645984c06b571750ea7e856812a948d857
adrissgoye <adrianaluciagoye@gmail.com>
2019-04-03 21:49:09 -0500

    listar trabajos

--- vistas/trabajos/admin.php
+++ vistas/trabajos/admin.php
@@ -30,11 +30,12 @@
         <form action="index.php?c=trabajos&a=create" method="post" autocomplete="off" enctype="multipart/form-data">
             <div class="card">
                   <div class="card-body">
+                   <div class="col-xs-6">
                         <div class="form-group is-empty">
                           <div class="input-group">
                             <span class="input-group-addon"><i class="zmdi zmdi-calendar"></i></span>
-                            <input type="text" class="form-control datepicker" id="datepicker-theme" placeholder="Pikaday dark..." aria-label="Use the arrow keys to pick a date" name="Trabajos[fechaInicio]"   value="" required>
-                         
+                            <input type="text" class="form-control datepicker" id="datepicker-theme" placeholder="Fecha inicio..." aria-label="Use the arrow keys to pick a date" name="Trabajos[fechaInicio]"   value="" required>
+                         </div>
                         </div>
                       </div>
                             <div class="form-group is-empty">
@@ -43,11 +44,43 @@
                             <input class="form-control datepicker" maxlength="45" type="text"  name="Trabajos[tipo]"   value="" required/>
                         </div>
                         </div>
-
+                          <div class="col-xs-6">
+                        <div class="form-group is-empty">
+                          <div class="input-group">
+                            <label >Costos</label>
+                            <input class="form-control datepicker" maxlength="45" type="text"  name="Trabajos[costos]" onkeypress="return numeros(event)"  value="" required/>
+                        </div>
+                        </div>
+                        </div>
                  <div class="form-group is-empty">
                           <div class="input-group">
                            <label >Trabajador</label>
                            <select class="select form-control" required="" name="Trabajos[id_trabajadores]">
+                           <option>Seleccion-</option>
+                               <?php foreach ( $trabajadores as $trabajador) {?>
+                           <option value="<?= $trabajador->id_trabajadores; ?>"><?=$trabajador->nombres; ?>  </option>
+                                    <?php } ?>
+                        </select>
+                          </div>
+             
+              </div>
+                <div class="col-xs-6">
+                  <div class="form-group is-empty">
+                          <div class="input-group">
+                           <label >Trabajador</label>
+                           <select class="select form-control" required="" name="Trabajos[id_trabajadores2]">
+                           <option>Seleccion-</option>
+                               <?php foreach ( $trabajadores as $trabajador) {?>
+                           <option value="<?= $trabajador->id_trabajadores; ?>"><?=$trabajador->nombres; ?>  </option>
+                                    <?php } ?>
+                        </select>
+                          </div>
+             </div>
+              </div>
+               <div class="form-group is-empty">
+                          <div class="input-group">
+                           <label >Trabajador</label>
+                           <select class="select form-control" required="" name="Trabajos[id_trabajadores3]">
                            <option>Seleccion-</option>
                                <?php foreach ( $trabajadores as $trabajador) {?>
                            <option value="<?= $trabajador->id_trabajadores; ?>"><?=$trabajador->nombres; ?>  </option>
--- vistas/trabajos/update.php
+++ vistas/trabajos/update.php
@@ -4,7 +4,7 @@
 	<title>Editar trabajos</title>
 <body>
 <a href="index.php?c=clientes&a=trabajos&id=<?= $_GET["id"] ?>"><h4>Volver</h4></a>
-<h2 align="center" >Actualizar trabajo</h2>
+<h2 align="center" >Actualizar trabajo <?= $trabajos->tipo ?></h2>
 
 <form method="post">
 <center>
@@ -19,13 +19,23 @@
                             <input maxlength="45" type="text"  name="Trabajos[tipo]"   
                             value="<?= $trabajos->tipo ?>" required/>
                              <br>
+                             <label >Costos</label>
+                            <input maxlength="45" type="text"  name="Trabajos[costos]"   
+                            value="<?= $trabajos->costos ?>" required/>
+                             <br>
                            <label >Trabajador</label>
                             <input maxlength="45" type="text"  name="Trabajos[id_trabajadores]"  
                             value="<?= $trabajos->id_trabajadores ?>" required/>
                             <br>
-                             <label >Cliente</label>
-                            <input maxlength="45" type="text"  name="Trabajos[id_clientes]" value="<?= $trabajos->id_clientes ?>" required/>
-                              <br>
+                             <label >Trabajador</label>
+                            <input maxlength="45" type="text"  name="Trabajos[id_trabajadores2]"  
+                            value="<?= $trabajos->id_trabajadores ?>" required/>
+                            <br>
+                            <label >Trabajador</label>
+                            <input maxlength="45" type="text"  name="Trabajos[id_trabajadores3]"  
+                            value="<?= $trabajos->id_trabajadores ?>" required/>
+                            <br>
+
                         
                            
                         <button type="submit" class="btn btn-primary">Guardar</button>
